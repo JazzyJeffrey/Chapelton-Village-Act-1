605cf75cba7c727a041b9c70ac3647d2578d59bc
JazzyJeffrey <jeff.renk3000@gmail.com>
2024-07-22 21:35:33 -0600

    Feat: Add dotenv and added access to Postgres database
    - Updating item/enemy system to use databse instead of browser storage

--- public/js/menus.js
+++ public/js/menus.js
@@ -1,53 +1,16 @@
-const showInventoryButton = document.getElementById("showInventoryButton");
-const closeInventoryButton = document.getElementById("closeInventoryButton");
-const inventoryWindow = document.getElementById("inventory");
-const playerSheet = document.getElementById("playerStats");
-const showPlayerSheetButton = document.getElementById("showPlayerStatsButton");
-const closePlayerSheetButton = document.getElementById("closePlayerStatsButton");
-let inventory = [];
 
 
-function initalizewEventListeners() {
+// Player Character
+const player = {
+    xp: 0,
+    level: 1,
+    hp: 100,
+    mp: 100,
+    strength: 1,
+    constitution: 1,
+    intellingence: 1,
+    luck: 1,
+    gold: 0,
+    currentWeapon: 0
+}
 
-    showInventoryButton.addEventListener("click", () => {
-        inventoryWindow.showModal();
-    });
-
-    closeInventoryButton.addEventListener("click", () => {
-        inventoryWindow.close();
-    });
-
-
-
-    showPlayerSheetButton.addEventListener("click", () => {
-        playerSheet.showModal();
-    });
-
-    closePlayerSheetButton.addEventListener("click", () => {
-        playerSheet.close();
-    });
-
-};
-
-
-
-
-function showInventory() {
-    const currentInventory = document.getElementById("currentInventory");
-    if(inventory.length >= 1) {
-        inventory.forEach(element => {
-            const inventoryItem = document.createElement("li"); 
-            inventoryItem.textContent = element;
-            currentInventory.appendChild(inventoryItem); 
-        });
-    } else {
-        currentInventory.innerHTML = ("Your inventory is empty.")
-    };
-};
-
-
-    initalizewEventListeners();
-    showInventory();
-
-
-    
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
+// Express Server connection
 const express = require('express');
 const app = express();
 const path = require('path');
+// Database connection
+require('dotenv').config();
+const { Client } = require('pg');
 
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
@@ -18,3 +22,18 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+
+
+
+const client = new Client({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+});
+
+client.connect()
+    .then(() => console.log("Connected to database"))
+    .catch(err => console.log("Connection error", err.stack));
